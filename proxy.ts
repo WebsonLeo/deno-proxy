5c8ccbc75350106537d08f07ce7e0a4c43c26d8e
WebsonLeo <webson_leo@outlook.com>
2023-07-18 20:29:52 +0800

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,16 @@
 import { Server } from "https://deno.land/std/http/server.ts";
 
-const TARGET = "www.google.com";
+const TARGET = "https://api.openai.com";
 const handler = async (request: Request) => {
   const url = new URL(request.url);
-  url.host = TARGET;
-  return await fetch(url, request);
-  // const url = new URL(request.url);
-  // const targetUrl = new URL(TARGET + url.pathname + url.search);
-  // console.log(request.headers)
-  // return await fetch(targetUrl.toString(), {
-  //   method: request.method,
-  //   headers: request.headers,
-  //   redirect: request.redirect,
-  //   body: request.body,
-  // });
+  const targetUrl = new URL(TARGET + url.pathname + url.search);
+  console.log(request.headers)
+  return await fetch(targetUrl.toString(), {
+    method: request.method,
+    headers: request.headers,
+    redirect: request.redirect,
+    body: request.body,
+  });
 };
 
 const server = new Server({ handler });
